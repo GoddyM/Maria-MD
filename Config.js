00d98a0f4b14e3007e643e861d706277345a1943
GpRoBoT🤖 <67107848+GoddyM@users.noreply.github.com>
2024-04-16 15:03:23 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,16 +2,16 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['2347012526311']
+global.ownername = "GpRoBoT🤖"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.location = "Nigeria"
 
 global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.stickername = 'GpStickers🤖'
 global.packname = 'Sticker By'
 global.author = 'Maria Bot'
 //console view/theme
